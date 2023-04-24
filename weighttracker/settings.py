870ff4a9967bcaa1871fb77a70b1e61194951abf
chrisPC <broman_chris@hotmail.com>
2023-04-24 14:11:41 -0400

    added python-dotenv for secret key

--- weighttracker/settings.py
+++ weighttracker/settings.py
@@ -1,5 +1,8 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,12 +12,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-gn#d2w@$l)fmxb7t@yr^ah0t0ywg=$n(aw^0c!$lqm%0@^2xk5"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['babytracker.local', 'www.babytracker.local']
 
 
 # Application definition
